94f3d4b35d0529f61232d6aec8b4cef610f3caea
Sai Patel <saikumaraggu6@gmail.com>
2025-07-17 18:23:40 +0530

    creaded crud operations

--- public/script.js
+++ public/script.js
@@ -1,26 +1,31 @@
-
-
+        render();    
     async function addTodo(){
-       
     let valueEl = document.getElementById('input');
     let Value =valueEl.value;
-    await axios.post('http://localhost:3004/adds',
-        { task : Value})
-
+ 
+    await axios.post('http://localhost:3005/adds',
+        {task : Value})
     valueEl.value='';
+      render(); 
+    };
 
-    const res= await axios.get('http://localhost:3004/data');
+
+    async function render(){
+    let HTML='';
+    const res= await axios.get('http://localhost:3005/data');
     const dataSet=res.data;
     dataSet[3].finance.forEach((hello)=> {
 
-    let div =document.createElement('div');
-    let parGRA = document.createElement('p');
-
-    console.log('value ar e',hello.task);
-    
-    parGRA.textContent=hello.task;
-    div.appendChild(parGRA);
-   document.body.appendChild(div);
+   HTML = HTML + `<div> <p>${hello.task}${hello.id} <button onclick='deleted(${hello.id})'>Delete</button> </p>
+   
+   </div>`
  });
+ document.querySelector('.todo-view').innerHTML=HTML;
 };
+
+
+async function deleted(Id){
+    await axios.delete(`http://localhost:3005/delete/${Id}`);
+     render(); 
+    }
  
